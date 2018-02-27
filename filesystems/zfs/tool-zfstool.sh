013f4b61fa21e0e596acc2a8406db643ad8c7b5a
Chris A. Giorgi <chrisgiorgi@gmail.com>
2018-02-27 06:50:45 +0000

    zfstool: Start 'zfstab' gen/parse.

--- filesystems/zfs/tool-zfstool.sh
+++ filesystems/zfs/tool-zfstool.sh
@@ -35,10 +35,13 @@ zfstool() {
 	command="$1"
 
 	case "$1" in
+		status) shift; zfstool_zpool_status && return 0 ;;
 		list) shift; zfstool_zfs_list && return 0 ;;
+		_parse) shift; _zfstool_parse_zfstab && return 0 ;;
+		_gen) shift; _zfstool_gen_zfstab && return 0 ;;
 		help) zfstool_usage && return 0 ;;
-		--*) warning "Unhandled global option '$1'!" ; return 1 ;;
-		*) warning "Unknown command '$1'!" ; return 1 ;;
+		--*) warning "Unhandled global option '$1'!" ; zfstool_usage ; return 1 ;;
+		*) warning "Unknown command '$1'!" ; zfstool_usage ; return 1 ;;
 	esac
 	return 1
 }
@@ -58,6 +61,7 @@ cat <<EOF
 Usage: zfstool <global opts> <command> <command opts>
 
 Commands:
+	status	get pool status
 	list	list existing ZFS datasets
 	help	show this help
 
@@ -67,3 +71,89 @@ EOF
 zfstool_zfs_list() {
 	"$_zfstool_zfs" list
 }
+
+zfstool_zpool_status() {
+	"$_zfstool_zpool" status
+}
+
+### Format:
+#	filesystem	canmount	name	mountpoint	options
+#	volume	size	name	sparse-vol	options
+_zfstool_parse_zfstab() {
+	#"$_zfstool_zfs" list -H -p -o type,canmount,name,mountpoint | awk '
+	awk '
+		BEGIN { FS="\t" ; split("", datasets) }
+		/^filesystem/	{ datasets[$3]=$3 ; type[$3]=$1 ; canmount[$3]=$2 ; filesystems[$3]=$3 ; mountpoint[$3]=$4 ; options[$3]=$5 }
+		/^volume/	{ datasetsp[$3]=$3 ; type[$3]=$1 ; size[$3]=$2 ; volumes[$3]=$3 ; sparse[$3]=$4 ; options[$3]=$5 }
+		END {
+			for ( fs in filesystems ) {
+				opts=options[fs]
+				if (canmount[fs] != "" && canmount[fs] != "inherit" && canmount[fs] != "default" ) { opts="canmount=" canmount[fs] " " opts }
+				if (mountpoint[fs] != "" && mountpoint[fs] != "inherit") { opts="mountpoint=\"" mountpoint[fs] "\" " opts }
+				gsub(/[[:alnum:]]+=/, "-o &", opts)
+				printf("zfs create %s\"%s\"\n", opts, fs)
+
+			}
+			for ( vol in volumes ) {
+				opts=options[vol]
+				gsub(/[[:alnum:]]+=/, "-o &", opts)
+				if ( sparse[vol]=="sparse" ) { sparseflag="-s " } else { sparseflag="" }
+				printf("zfs create %s%s-V %s \"%s\"\n", sparseflag, opts, size[vol], vol)
+			}
+		}
+	'
+}
+
+### Format:
+#	dataset	option	value	source
+_zfstool_gen_zfstab() {
+	"$_zfstool_zfs" list -H -p -o type,name | awk '
+		BEGIN { OFS="\t" ; split("", datasets) }
+
+		{
+			datasets[$2]=$2 ; type[$2]=$1 ; name[$2]=$2
+			if ( $1 == "filesystem" ) {
+					cmd="( '"$_zfstool_zfs"' get mountpoint -H -p -s local -t filesystem " $2 " | cut -f3 )"
+					cmd | getline mountpoint[$2]
+					close(cmd)
+					cmd="( '"$_zfstool_zfs"' get canmount -H -p -t filesystem " $2 " | cut -f4 )"
+					cmd | getline canmountsrc[$2]
+					close(cmd)
+					if ( canmountsrc[$2] == "local" ) {
+						cmd="( '"$_zfstool_zfs"' get canmount -H -p -s local -t filesystem " $2 " | cut -f3 )"
+						cmd | getline canmount[$2]
+						close(cmd)
+					} else if ( canmountsrc[$2] == "inherited" ) {
+						canmount[$2]="inherit"
+					} else { canmount[$2]="default" }
+
+					cmd="( '"$_zfstool_zfs"' get all -H -p -s local -t filesystem " $2 " | cut -f2,3 --output-delimiter=\"=\" | grep -v -e \"mountpoint=\" -e \"canmount=\" | tr \"\n\" \" \")"
+					cmd | getline options[$2]
+					close(cmd)
+
+			} else if ( $1 == "volume" ) {
+					cmd="( '"$_zfstool_zfs"' get volsize -H -p -s local -t volume " $2 " | cut -f3 )"
+					cmd | getline volsize[$2]
+					close(cmd)
+					cmd="( '"$_zfstool_zfs"' get refreservation -H -p -s local -t volume " $2 " | cut -f3 )"
+					cmd | getline refreservation[$2]
+					close(cmd)
+					if ( refreservation[$2] < volsize[$2] ) { sparse[$2]="sparse" }
+					cmd="( '"$_zfstool_zfs"' get all -H -p -s local -t volume " $2 " | cut -f2,3 --output-delimiter=\"=\" | grep -v \"volsize=\" | tr \"\n\" \" \")"
+					cmd | getline options[$2]
+					close(cmd)
+			}
+
+		}
+		END {
+			for ( ds in datasets ) {
+				if ( type[ds] == "filesystem" ) {
+					print type[ds] "\t" canmount[ds] "\t" name[ds] "\t" mountpoint[ds] "\t" options[ds]
+				} else if ( type[ds] == "volume" ) {
+					print type[ds] "\t" volsize[ds] "\t" name[ds] "\t" sparse[ds] "\t" options[ds]
+				}
+			}
+		}
+	'
+}
+
